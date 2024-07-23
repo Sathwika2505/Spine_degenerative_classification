0407fddd77ef29478e3fb8753916f61b113d71eb
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-07-23 16:27:24 +0530

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -1,38 +1,14 @@
-from pathlib import Path
-import os
-import gc
-import sys
-from PIL import Image
-import cv2
-import math, random
-import numpy as np
 import pandas as pd
-from glob import glob
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold
-
-from collections import OrderedDict
 import boto3
 import zipfile
 from io import BytesIO
-import pandas as pd
+import os
+import numpy as np
+from PIL import Image
+import glob
 import torch
-import torch.nn.functional as F
-from torch import nn
-from torch.utils.data import DataLoader, Dataset
-from torch.optim import AdamW
-
-import timm
-from transformers import get_cosine_schedule_with_warmup
-
+from torch.utils.data import Dataset
 import albumentations as A
-import torchvision
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, Dataset
-import requests
-from torchvision.datasets import ImageFolder
-from torchvision import datasets
 import dill as pickle
 
 def transform_data():
@@ -55,9 +31,9 @@ def transform_data():
     df = df.fillna(-100)
     label2id = {'Normal/Mild': 0, 'Moderate': 1, 'Severe': 2}
     df = df.replace(label2id)
-    
+
     AUG_PROB = 0.75
-    IMG_SIZE = [512, 512]
+    IMG_SIZE = [256, 256]
     IN_CHANS = 30
     
     data_transform = A.Compose([
@@ -76,7 +52,7 @@ def transform_data():
         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=AUG_PROB),
         A.Resize(IMG_SIZE[0], IMG_SIZE[1]),
         A.CoarseDropout(max_holes=16, max_height=64, max_width=64, min_holes=1, min_height=8, min_width=8, p=AUG_PROB),    
-        A.Normalize(mean=0.5, std=0.5)
+        A.Normalize(mean=0.5, std=0.5,)
     ])
 
     class RSNA24Dataset(Dataset):
@@ -84,11 +60,14 @@ def transform_data():
             self.df = df
             self.transform = transform
             self.phase = phase
+            self.path = f'{os.getcwd()}/cvt_jpg/'
             self.set_classes()
-            print("===============df : ", df)
+            #print("===============df : ", df.columns)
+            print("Classes after set_classes:", self.classes)  # Debugging statement
         
         def set_classes(self):
             self.classes = {col: idx for idx, col in enumerate(self.df.columns[1:], start=1)}
+            print("Classes set in set_classes method:", self.classes)  # Debugging statement
         
         def __len__(self):
             return len(self.df)
@@ -97,64 +76,46 @@ def transform_data():
             x = np.zeros((512, 512, IN_CHANS), dtype=np.uint8)
             t = self.df.iloc[idx]
             st_id = int(t['study_id'])
-            label = t.iloc[1:].values.astype(np.int64)
+            labels = t.iloc[1:].values.astype(np.int64)
+            
             print("st_id----------", st_id)
-            print("label----------", label)
-            # Sagittal T1
-            for i in range(0, 10, 1):
-                try:
-                    p = f'./cvt_jpg/{st_id}/Sagittal T1/{i:03d}.jpg'
-                    img = Image.open(p).convert('L')
+            print("labels----------", labels)
+            
+            # Adjust paths based on new file structure
+            for ds in ['Sagittal T1', 'Sagittal T2_STIR', 'Axial T2']:
+                folder = ds.replace('/', '_')
+                allimgs = glob.glob(f'{self.path}/{folder}/{st_id}_*.jpg')
+                allimgs = sorted(allimgs)
+                
+                if len(allimgs) == 0:
+                    print(f'{st_id} {ds} has no images')
+                    continue
+    
+                for j, impath in enumerate(allimgs[:10]):
+                    img = Image.open(impath).convert('L')
                     img = np.array(img)
-                    x[..., i] = img.astype(np.uint8)
-                except:
-                    print(f'failed to load on {st_id}, Sagittal T1')
-                    pass
+                    x[..., j] = img.astype(np.uint8)
                 
-            # Sagittal T2/STIR
-            for i in range(0, 10, 1):
-                try:
-                    p = f'./cvt_jpg/{st_id}/Sagittal T2_STIR/{i:03d}.jpg'
-                    img = Image.open(p).convert('L')
-                    img = np.array(img)
-                    x[..., i+10] = img.astype(np.uint8)
-                except:
-                    print(f'failed to load on {st_id}, Sagittal T2/STIR')
-                    pass
+                if self.transform is not None:
+                    x = self.transform(image=x)['image']
+              
+                x = x.transpose(2, 0, 1)
                 
-            # Axial T2
-            axt2 = glob.glob(f'./cvt_jpg/{st_id}/Axial T2/*.jpg')
-            axt2 = sorted(axt2)
-        
-            step = len(axt2) / 10.0
-            st = len(axt2)/2.0 - 4.0*step
-            end = len(axt2)+0.0001
-                    
-            for i, j in enumerate(np.arange(st, end, step)):
-                try:
-                    p = axt2[max(0, int((j-0.5001).round()))]
-                    img = Image.open(p).convert('L')
-                    img = np.array(img)
-                    x[..., i+20] = img.astype(np.uint8)
-                except:
-                    print(f'failed to load on {st_id}, Sagittal T2/STIR')
-                    pass  
+                # Prepare labels in the format "column name: label"
+                label_dict = {col: labels[i] for i, col in enumerate(self.df.columns[1:])}
+                label_formatted = [(col, label_dict[col]) for col in self.df.columns[1:]]
+                #print("label_formatted ============",label_formatted)
                 
-            assert np.sum(x) > 0
-                
-            if self.transform is not None:
-                x = self.transform(image=x)['image']
-      
-            x = x.transpose(2, 0, 1)
-                    
-            return x, label
+                return x, label_formatted
 
     model_dataset = RSNA24Dataset(df, phase='train', transform=data_transform)
-    img, ann = model_dataset[1]
+    #for i in range(len(model_dataset)):
+    img, ann = model_dataset[i]
     print("iiiiiiii", img)
     print("aaaaaaaaa:", ann)
+        
     with open('spine-degenerative-mriscan-classification.pkl', 'wb') as f:
         pickle.dump(model_dataset, f)
     return model_dataset
-    
+
 transform_data()
