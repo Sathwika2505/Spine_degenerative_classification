9941447a2d332e8effb08366a0830e6e55fd61c9
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-07-23 16:27:02 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -3,19 +3,19 @@ import boto3
 import zipfile
 from io import BytesIO
 import pydicom
-from PIL import Image
-import random
-import shutil
-import glob, os
+import glob
+import os
 import numpy as np
 import cv2
 from tqdm import tqdm
 import re
+import random
+import shutil
 
 # Define paths
 rd = os.path.join(os.getcwd(), "extracted_files")
-
 visualization_path = "./visualization"
+output_root_path = "./cvt_jpg"
 
 def read_csv_from_s3(csv_filename):
     s3 = boto3.client('s3')
@@ -30,7 +30,7 @@ def read_csv_from_s3(csv_filename):
                 print("CSV file read successfully.")
                 print(csv_data.head())
     return csv_data
-                
+
 csv_filename1 = 'train_label_coordinates.csv' 
 csv_filename2 = 'train_series_descriptions.csv' 
 
@@ -45,7 +45,7 @@ def atoi(text):
 
 def natural_keys(text):
     return [atoi(c) for c in re.split(r'(\d+)', text)]
-    
+
 def imread_and_imwrite(src_path, dst_path):
     dicom_data = pydicom.dcmread(src_path)
     image = dicom_data.pixel_array
@@ -55,7 +55,6 @@ def imread_and_imwrite(src_path, dst_path):
     cv2.imwrite(dst_path, img)
 
 desc = list(df['series_description'].unique())
-
 st_ids = df['study_id'].unique()
 print(st_ids[:3], len(st_ids))
 
@@ -64,7 +63,7 @@ for idx, si in enumerate(tqdm(st_ids, total=len(st_ids))):
     for ds in desc:
         ds_ = ds.replace('/', '_')
         pdf_ = pdf[pdf['series_description'] == ds]
-        os.makedirs(f'cvt_jpg/{si}/{ds_}', exist_ok=True)
+        os.makedirs(f'{output_root_path}/{ds_}', exist_ok=True)
         allimgs = []
         for i, row in pdf_.iterrows():
             pimgs = glob.glob(f'{rd}/train_images/{row["study_id"]}/{row["series_id"]}/*.dcm')
@@ -77,7 +76,7 @@ for idx, si in enumerate(tqdm(st_ids, total=len(st_ids))):
 
         if ds == 'Axial T2':
             for j, impath in enumerate(allimgs):
-                dst = f'cvt_jpg/{si}/{ds}/{j:03d}.jpg'
+                dst = f'{output_root_path}/{ds_}/{si}_{j:03d}.jpg'
                 imread_and_imwrite(impath, dst)
 
         elif ds == 'Sagittal T2/STIR':
@@ -85,13 +84,13 @@ for idx, si in enumerate(tqdm(st_ids, total=len(st_ids))):
             st = len(allimgs) / 2.0 - 4.0 * step
             end = len(allimgs) + 0.0001
             for j, i in enumerate(np.arange(st, end, step)):
-                dst = f'cvt_jpg/{si}/{ds_}/{j:03d}.jpg'
+                dst = f'{output_root_path}/{ds_}/{si}_{j:03d}.jpg'
                 ind2 = max(0, int((i - 0.5001).round()))  
                 imread_and_imwrite(allimgs[ind2], dst)
 
             # Debug print
-            jpg_files = glob.glob(f'cvt_jpg/{si}/{ds_}/*.jpg')
-            print(f'Directory: cvt_jpg/{si}/{ds_}/')
+            jpg_files = glob.glob(f'{output_root_path}/{ds_}/*.jpg')
+            print(f'Directory: {output_root_path}/{ds_}/')
             print(f'Number of .jpg files: {len(jpg_files)}')
 
         elif ds == 'Sagittal T1':
@@ -99,43 +98,36 @@ for idx, si in enumerate(tqdm(st_ids, total=len(st_ids))):
             st = len(allimgs) / 2.0 - 4.0 * step
             end = len(allimgs) + 0.0001
             for j, i in enumerate(np.arange(st, end, step)):
-                dst = f'cvt_jpg/{si}/{ds}/{j:03d}.jpg'
+                dst = f'{output_root_path}/{ds_}/{si}_{j:03d}.jpg'
                 ind2 = max(0, int((i - 0.5001).round()))
                 imread_and_imwrite(allimgs[ind2], dst)
 
             # Debug print
-            jpg_files = glob.glob(f'cvt_jpg/{si}/{ds}/*.jpg')
+            jpg_files = glob.glob(f'{output_root_path}/{ds_}/*.jpg')
             if len(jpg_files) != 10:
-                print(f'Warning: Expected 10 .jpg files, but found {len(jpg_files)} in {si}/{ds}')
+                print(f'Warning: Expected 10 .jpg files, but found {len(jpg_files)} in {ds_}')
                 continue
                 
-            print(f'Directory: cvt_jpg/{si}/{ds}/')
+            print(f'Directory: {output_root_path}/{ds_}/')
             print(f'Number of .jpg files: {len(jpg_files)}')
 
-output_root_path = "./cvt_jpg"
+# Select random images from 3 different folders
+random_labels = random.sample(desc, 3)
 
-study_ids = [d for d in os.listdir(output_root_path) if os.path.isdir(os.path.join(output_root_path, d))]
-
-# Select a random study_id
-selected_study_id = random.choice(study_ids)
-print(f"Selected study_id: {selected_study_id}")
-
-# Construct the path for the selected study_id
-study_path = os.path.join(output_root_path, selected_study_id)
-if os.path.isdir(study_path):
-    for desc_folder in os.listdir(study_path):
-        desc_path = os.path.join(study_path, desc_folder)
-        if os.path.isdir(desc_path):
-            jpg_files = [f for f in os.listdir(desc_path) if f.endswith('.jpg')]
-
-            if jpg_files:  # Ensure the folder is not empty
-                random_file = random.choice(jpg_files)  # Select one random file
-
-                jpg_path = os.path.join(desc_path, random_file)
-                new_jpg_path = os.path.join(os.getcwd(), f"{desc_folder}_{random_file}")
-
-                # Copy the JPG file to the current working directory
-                shutil.copy2(jpg_path, new_jpg_path)
-                #print(f"Copied {jpg_path} to {new_jpg_path}")
+for label in random_labels:
+    label_ = label.replace('/', '_')
+    label_path = os.path.join(output_root_path, label_)
+    if os.path.isdir(label_path):
+        jpg_files = [f for f in os.listdir(label_path) if f.endswith('.jpg')]
+        
+        if jpg_files:  # Ensure the folder is not empty
+            random_file = random.choice(jpg_files)  # Select one random file
+            
+            jpg_path = os.path.join(label_path, random_file)
+            new_jpg_path = os.path.join(os.getcwd(), f"{label_}_{random_file}")
+            
+            # Copy the JPG file to the current working directory
+            shutil.copy2(jpg_path, new_jpg_path)
+            print(f"Copied {jpg_path} to {new_jpg_path}")
 
 print("Script finished successfully.")
